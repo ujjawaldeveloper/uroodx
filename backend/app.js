94b8cf0817764e868235d76c08da4310a1e75cb9
ujjawaldeveloper <uy2110101@gmail.com>
2025-03-06 11:18:04 +0530

    backend working successfully

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const cors = require("cors");
 const mongoose = require("mongoose");
 const routes = require("./routes");
+const cors = require("cors");
 require("dotenv").config();
 const app = express();
 app.use(cors());
